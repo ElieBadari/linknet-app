058084778b40dc667cdd832693124f192d6b5912
ElieBadari <elie.badari@hotmail.com>
2022-05-05 12:56:05 +0300

    finished login page

--- LinkNet-app/app/src/main/java/com/example/linknet/SplashScreen.java
+++ LinkNet-app/app/src/main/java/com/example/linknet/SplashScreen.java
@@ -15,7 +15,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
         handler = new Handler();
         intent = new Intent(this, LoginPage.class);
 
